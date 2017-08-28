7a117aeb348548270bee6b246398e513e6c51b24
CHKass <kentc@btinternet.com>
2017-08-28 21:28:59 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -2,8 +2,10 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnscroll from './modules/RevealOnscroll';
 import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu();
 new RevealOnscroll($(".feature-item"), "85%");
 new RevealOnscroll($(".testimonial"), "60%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
